fe9ba06f349614a4a649cd578ca0dff62f7039c8
unknown <tecmatheusg@hotmail.com>
2023-06-10 13:51:20 -0300

    Waiters explicitos e implicitos

---
+++ Alura.LeilaoOnline.Selenium/Testes/AoOfertarLance.cs
@@ -0,0 +1,46 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoOfertarLance
+    {
+        private IWebDriver driver;
+
+        public AoOfertarLance(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginInteressadaDeveAtualizarLanceAtual()
+        {
+            //Arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("fulano@example.org", "123");
+            loginPO.SubmeteFormulario();
+
+            var detalhePO = new DetalheLeilaoPO(driver);
+            detalhePO.Visitar(2);
+
+            //Act
+            detalhePO.OfertarLance(300);
+
+            //Assert
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+            var iguais = wait.Until(driver => detalhePO.LanceAtual == 300);
+
+            Assert.True(iguais);
+        }
+    }
+}
